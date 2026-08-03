93d96675845d82803c164b2b0791063181f74113
Jarelle Calimlim <jarellecalimlim25@gmail.com>
2026-08-03 09:20:17 +0800

    Practical Exam Part 4: JSX expressions implemented

--- src/components/inventory/Item.jsx
+++ src/components/inventory/Item.jsx
@@ -1,10 +1,25 @@
-function Item({ name , category, quantity }) {
-    return(
-        <div>
-            <h3>{name}</h3>
-            <p>Category: {category}</p>
-            <p>Quantity: {quantity}</p>
-        </div>
-    );
+function Item({ name, quantity }) {
+  const category = 'Computer Equipment';
+  const unitPrice = 50;
+  const isAvailable = quantity > 0;
+
+  function getItemCode() {
+    return name.substring(0, 3).toUpperCase();
+  }
+
+  return (
+    <article>
+      <h3>{name}</h3>
+
+      <p>Item Code: {getItemCode()}</p>
+
+      <p>Category: {`${category} - ${name}`}</p>
+
+      <p>Stock Value: ${quantity * unitPrice}</p>
+
+      <p>{isAvailable ? 'Available' : 'Out of Stock'}</p>
+    </article>
+  );
 }
+
 export default Item;
